ff64ff16d4ff49419235822256f0bc04111c46a2
NishidaYujiro <b173033@hiroshima-u.ac.jp>
2020-10-31 17:40:09 +0900

    ヘッダーファイルの追加

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,18 @@
 <template>
   <div id="app">
-    <button v-on:click="login">login</button>
+    <!-- <button v-on:click="login">login</button> -->
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <CustomHeader/>
   </div>
 </template>
 
 <script>
-import firebase from 'firebase/app';
-import 'firebase/auth'
+import CustomHeader from './components/CustomHeader.vue'
 export default {
   name: 'App',
-  methods:{
-    login: function(){
-      console.log("login")
-      var provider = new firebase.auth.GithubAuthProvider();
-      firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-        // var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        console.log(user)
-        // ...
-      }).catch(function() {
-        // Handle Errors here.
-        // var errorCode = error.code;
-        // var errorMessage = error.message;
-        // The email of the user's account used.
-        // var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        // var credential = error.credential;
-        // ...
-      });
-    }
+  components: {
+    CustomHeader
   }
 }
 </script>
---
+++ src/components/CustomHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="header">
+    <button v-on:click="login">login</button>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase/app';
+import 'firebase/auth'
+export default {
+  name: 'CustomHeader',
+  methods:{
+    login: function(){
+      console.log("login")
+      var provider = new firebase.auth.GithubAuthProvider();
+      firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        // var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        console.log(user)
+        // ...
+      }).catch(function() {
+        // Handle Errors here.
+        // var errorCode = error.code;
+        // var errorMessage = error.message;
+        // The email of the user's account used.
+        // var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        // var credential = error.credential;
+        // ...
+      });
+    }
+  }
+}
+</script>
