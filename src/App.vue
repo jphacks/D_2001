7e114e85e889f8acad1d057ec7883d445777a5de
kugimasa <kugimania@gmail.com>
2020-10-31 17:14:44 +0900

    投稿フォームのComponent作成

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <InputForm/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import InputForm from './components/InputForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    InputForm
   }
 }
 </script>
---
+++ src/components/InputForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="input-form">
+    <p>タイトル</p>
+      <input v-model="title" placeholder="タイトル">
+    <p>詳細</p>
+      <textarea v-model="description" placeholder="説明を記入してください"></textarea>
+    <p>選択肢</p>
+    <div v-for="(option,index) in optionList" :key="index" class="option">
+      <input v-model="option.text" placeholder="選択肢">
+    </div>
+    <button v-on:click="addOption">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputForm',
+  data() {
+    return {
+      optionList: []
+    };
+  },
+  methods: {
+    addOption() {
+      const option ={
+        text: "",
+      }
+      this.optionList.push(option);
+      console.log(this.optionList);
+    }
+  }
+}
+</script>
