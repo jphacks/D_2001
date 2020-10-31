066010e1d766ffbd3c8bb367a9a527b0b4802c34
Yamakatsu63 <yamakatsu1217.63@gmail.com>
2020-10-31 16:19:54 +0900

    GitHub認証によるログイン

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,31 @@
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-
+import firebase from 'firebase/app';
+import 'firebase/auth'
 export default {
   name: 'App',
-  // components: {
-  //   HelloWorld
-  // }
   methods:{
     login: function(){
       console.log("login")
+      var provider = new firebase.auth.GithubAuthProvider();
+      firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        // var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        console.log(user)
+        // ...
+      }).catch(function() {
+        // Handle Errors here.
+        // var errorCode = error.code;
+        // var errorMessage = error.message;
+        // The email of the user's account used.
+        // var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        // var credential = error.credential;
+        // ...
+      });
     }
   }
 }
