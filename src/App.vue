68b2396fe52e61f91397bf5346d7e6e32a397b92
tyanio <tyanio1996@gmail.com>
2020-11-01 17:50:03 +0900

    投稿ページを微調整しました

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <CustomHeader/>
-    <InputForm/>
+    <div id="contents-container">
+      <InputForm/>
+    </div>
   </div>
 </template>
 
@@ -24,5 +26,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #333333;
+  background-color: #F6F6F4;
+  min-height: 100vh;
+  padding-top: 64px;
+}
+
+#contents-container {
+  max-width: 1140px;
+  margin-top: 30px;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: #FFFFFF;
 }
 </style>
