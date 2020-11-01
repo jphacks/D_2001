312447361ba8c9eaa6ad52add83415e1288a5020
Yamakatsu63 <yamakatsu1217.63@gmail.com>
2020-11-01 11:52:24 +0900

    起動時にログイン状態を確認 & ログイン状態によって表示を変える

--- src/components/CustomHeader.vue
+++ src/components/CustomHeader.vue
@@ -1,6 +1,10 @@
 <template>
-  <div id="header">
-    <button v-on:click="login">login</button>
+  <div id="header" v-if="!loading">
+    <button v-on:click="login" v-if="!isUserExist">login</button>
+    <button v-on:click="logout">logout</button>
+    <div v-if="isUserExist">
+      <p> {{ userName }} </p>
+    </div>
   </div>
 </template>
 
@@ -9,34 +13,56 @@ import firebase from 'firebase/app';
 import 'firebase/auth'
 export default {
   name: 'CustomHeader',
-  created: function(){
-    var user = firebase.auth().currentUser;
-    if (user != null) {
-      console.log("ログイン済み "+user.displayName)
+  data() {
+    return {
+      isUserExist: false,
+      userName: String,
+      currentuser: firebase.auth().currentUser,
+      loading: true,
     }
+  },
+  mounted: function(){
+    // userのステータスが変わったとき（ログイン，ログアウト時）にconfirmLoginを実行する
+    firebase.auth().onAuthStateChanged(this.confirmLogin);
   },
   methods:{
     login: function(){
       console.log("login")
       var provider = new firebase.auth.GithubAuthProvider();
       firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-        // var token = result.credential.accessToken;
-        // The signed-in user info.
+        // ログイン成功
         var user = result.user;
         console.log(user.displayName)
-        // ...
+        this.isUserExist = true
+        this.userName = user.displayName 
       }).catch(function() {
-        // Handle Errors here.
-        // var errorCode = error.code;
-        // var errorMessage = error.message;
-        // The email of the user's account used.
-        // var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        // var credential = error.credential;
-        // ...
+        // ログイン失敗
       });
     },
-  }
+    //デバッグ用のサインアウトメソッド
+    logout: function(){
+      firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+        console.log("サインアウト")
+      }).catch(function(error) {
+        // An error happened.
+        console.log("サインアウトエラー")
+        console.log(error.message)
+      });
+    },
+    confirmLogin: function(user){
+      // ユーザがログイン状態か確認する
+      if (user) {
+        console.log("ログイン済み "+ user.displayName)
+        this.isUserExist = true
+        this.userName = user.displayName
+      } else {
+        console.log("未ログイン")
+        this.isUserExist = false
+      }
+      // ログイン確認終了
+      this.loading = false
+    },
+  },
 }
 </script>
