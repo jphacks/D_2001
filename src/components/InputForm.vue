0f27110c4dc301f5fe09ae5faf95af4e1a7e6ebf
Yamakatsu63 <yamakatsu1217.63@gmail.com>
2020-11-01 10:43:01 +0900

    エラーの修正

--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="input-form">
     <p>タイトル</p>
-      <input v-model="title" placeholder="タイトル">
+      <input placeholder="タイトル">
     <p>詳細</p>
-      <textarea v-model="description" placeholder="説明を記入してください"></textarea>
+      <textarea placeholder="説明を記入してください"></textarea>
     <p>選択肢</p>
     <div v-for="(option,index) in optionList" :key="index" class="option">
       <input v-model="option.text" placeholder="選択肢">
