3188eb2bfc1016bec9407c12312d2177d9507590
NishidaYujiro <b173033@hiroshima-u.ac.jp>
2020-11-02 12:36:44 +0900

    詳細ページに値を渡す

---
+++ src/components/DetailsPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <CustomHeader/>
+     <div v-if="title != undefined">{{title}}</div>
+     <div v-if="description != undefined">{{description}}</div>
+  </div>
+</template>
+
+<script>
+import CustomHeader from './CustomHeader'
+export default {
+  name: 'DetailsPage',
+  components: {
+    CustomHeader,
+  },
+  props: [
+    'title',
+    'description'
+  ],
+}
+</script>
