0dc3cdb055a693125678d68656f20762da321a3c
TakedaHiroki <hirokitakedaxxx@gmail.com>
2020-11-02 17:21:41 +0900

    詳細ページで新しい回答候補を追加できるようにした

--- src/components/DetailsPage.vue
+++ src/components/DetailsPage.vue
@@ -8,6 +8,15 @@
           <td v-text="answer.text"></td>
         </tr>
       </table>
+      <div v-for="(h,index) in newAnswer" :key="index" class="option">
+        <input v-model="h.text" placeholder="選択肢">
+      </div>
+      <button v-on:click="addOption">+</button>
+      <br>
+      <button v-on:click="cancel">キャンセル</button>
+      <router-link to="/">
+        <button v-on:click="post">投稿</button>
+      </router-link>
   </div>
 </template>
 
@@ -19,10 +28,45 @@ export default {
   data: function() {
     return {
       answers: [],
+      newAnswer: [],
     }
   },
   components: {
     CustomHeader,
+  },
+  methods: {
+    addOption() {
+      const option ={
+        text: "",
+      }
+      this.newAnswer.push(option);
+      console.log(this.newAnswer);
+    },
+    cancel() {
+      console.log("キャンセルしました")
+    },
+    post() {
+      var newAnswer = this.newAnswer
+      db.collection("Questions").where("title", "==", this.title).where("description", "==", this.description)
+      .get()
+      .then(function(querySnapshot) {
+          querySnapshot.forEach(function(doc) {
+            console.log(doc.id);
+            newAnswer.forEach(function( value ) {
+              db.collection("Questions").doc(doc.id).collection("Answers").add({
+                content : value
+              })
+              console.log(value);
+            })
+          });
+      })
+      .then(function() {
+          console.log("Answer successfully written!");
+      })
+      .catch(function(error) {
+          console.log("Error getting documents: ", error);
+      });
+    },
   },
   mounted: function(){
     console.log(this.title)
